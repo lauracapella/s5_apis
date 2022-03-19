2f27fd5cc6f09220f64e5da879139a7b41abb202
Laura Capella <laura@laguapacomunicacion.com>
2022-03-19 19:45:30 +0100

    Nivel 1

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 const chiste_btn = document.querySelector('#chiste_btn');
 const chiste_div = document.querySelector('#chiste_div');
-const getData = function() {
+const score_btn = document.querySelector('#score_bot');
+
+let scoreUser = 0;
+
+let reportAcudits = [
+ /*  {
+    joke: "...",
+    score: 1,
+    date: ''
+  } */
+];
+
+let getValoration = function(value) {
+  if(value === 1){
+    scoreUser = 1;
+  }else if(value === 2){
+    scoreUser = 2;
+  }else if(value === 3){
+    scoreUser = 3;
+  };
+  return scoreUser;
+}
+
+const getData = function(scoreUser) {
+    const d = new Date();
+    let text = d.toISOString();
+
     chiste_div.innerText = "cargando..."
     fetch('https://icanhazdadjoke.com/', {
         headers: {
@@ -9,10 +35,11 @@ const getData = function() {
       })
         .then(res => res.json())
         .then(data => {
-            chiste_div.innerText = JSON.stringify(data.joke, null, 2)
+            chiste_div.innerText = JSON.stringify(data.joke, null, 2);
+            reportAcudits.push({joke: data.joke, date: text, score: getValoration()});
+            alert(reportAcudits[0].joke + reportAcudits[0].date + reportAcudits[0].score + reportAcudits.length);
         }) 
         .catch(error => console.log(error))
 };
+
 chiste_btn.addEventListener('click', getData);
-
-
