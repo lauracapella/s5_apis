c661b7aade539c6e2a63fa690d98c40e4e684528
Laura Capella <laura@laguapacomunicacion.com>
2022-03-20 10:35:47 +0100

    Nivel 2 - weather

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const chiste_btn = document.querySelector('#chiste_btn');
 const chiste_div = document.querySelector('#chiste_div');
 const score_btn = document.querySelector('#score_bot');
+const weather_div = document.querySelector('.tiempoHoy');
 
 let scoreUser = 0;
 
@@ -54,5 +55,20 @@ let getValoration = function(value) {
   alert(reportAcudits[0].joke + reportAcudits[0].date + reportAcudits[0].score + reportAcudits.length);
   return scoreUser;
 };
+
+
+
+async function fetchWeatherJSON() {
+  const response = await fetch('https://api.openweathermap.org/data/2.5/weather?lat=41.3874&lon=2.1686&appid=a408449b27687356ad659876873bd4dd');
+  const weather = await response.json();
+  return weather;
+}
+fetchWeatherJSON().then(weather => {
+  weather_div.innerHTML = (weather.weather[0].main); // fetched weather
+});
+ 
+
+//https://api.openweathermap.org/data/2.5/weather?lat=41.3874&lon=2.1686&appid=a408449b27687356ad659876873bd4dd
+//https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=a408449b27687356ad659876873bd4dd
 
 chiste_btn.addEventListener('click', getData);
